b1b331b671b9a9a7b3fa1b2cd29b1769db6102c0
Lada Kosova <lada.kosova.2020@gmail.com>
2025-05-19 22:51:55 +0800

    Война-и-Мир

--- word_statistics.sh
+++ word_statistics.sh
@@ -1,28 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Список исходных файлов
-INPUT_FILES="tolstoj_lew_nikolaewich-text_1.txt tolstoj_lew_nikolaewich-text_2.txt tolstoj_lew_nikolaewich-text_4.txt"
-# Имя результирующего файла
+INPUT_FILES="tolstoj_lew_nikolaewich-text_1.txt tolstoj_lew_nikolaewich-text_2.txt tolstoj_lew_nikolaewich-text_3.txt tolstoj_lew_nikolaewich-text_4.txt"
 OUTPUT_FILE="word_statistics.csv"
 
-# Обработка:
-# 1. Объединяем все файлы
-# 2. Переводим все буквы к нижнему регистру
-# 3. Заменяем всё, что не буквы или цифры, на пробел
-# 4. Разбиваем по словам (по одному на строку)
-# 5. Удаляем пустые строки
-# 6. Сортируем слова
-# 7. Считаем частоту каждого слова
-# 8. Сортируем по убыванию частоты
-# 9. Сохраняем в формате CSV (word,count)
-
+# Используем Perl для извлечения слов по UTF-8 (кириллица и латиница)
 cat $INPUT_FILES | \
 tr '[:upper:]' '[:lower:]' | \
-tr -c '[:alnum:]' '[\n*]' | \
-grep -v '^$' | \
+perl -CSD -nE 'while (/(\p{L}+)/gu) { say $1 }' | \
 sort | \
 uniq -c | \
-sort -nr | \
-awk 'BEGIN{OFS=","; print "word,count"} {print $2,$1}' > $OUTPUT_FILE
+sort -nr > tmp_wordstat.txt
 
-echo "Статистика сохранена в $OUTPUT_FILE"
+echo "word,count" > $OUTPUT_FILE
+awk '{print $2 "," $1}' tmp_wordstat.txt >> $OUTPUT_FILE
+
+rm tmp_wordstat.txt
